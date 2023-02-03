9e959139c6cdd503f37c07424e12f12dd7803bd8
linguopeng <linguopeng@cryptape.com>
2023-02-03 15:47:31 +0800

    add page info `test-id`

--- packages/e2e/src/nexus/page/notification-page.ts
+++ packages/e2e/src/nexus/page/notification-page.ts
@@ -1,3 +1,38 @@
 export const NotificationPageTextInfo = {
-    Approve:"Approve"
+    Approve: "Approve"
+}
+
+export const SignMessagePageTestIdInfo = {
+
+    /**
+     * test-id: reject, the rejrect button
+     * test-id: sign, the sign button
+     * test-id: password, the password input
+     * test-id: submitPassword, the submit password button
+     * test-id: url, the requester url
+     * tes-tid: message, the message for sigining
+     */
+
+    Reject: 'reject',
+    Sign: 'sign',
+    Password: 'password',
+    SubmitPassword: 'submitPassword',
+    Url: 'url',
+    Message: 'message'
+
+}
+
+export const GrantPageTestIdInfo = {
+
+    /**
+     * Anchors:
+     *
+     * test-id: grant the grant button
+     * test-id: reject the reject button
+     */
+
+    Grant:'grant',
+    Reject:'reject'
+
+
 }
--- packages/e2e/src/nexus/page/wallet-manager-page.ts
+++ packages/e2e/src/nexus/page/wallet-manager-page.ts
@@ -1,20 +1,120 @@
-
 export const WalletManagerPageTextInfo = {
-    ImportWallet:"Import wallet",
-    CreateANewWallet:"Create a new wallet",
+    ImportWallet: 'Import wallet',
+    CreateANewWallet: 'Create a new wallet',
 }
 export const WalletManagerPageXpathInfo = {
-    getSeedByIdx:(num:number)=> `//div/div[${num}]/span`
+    getSeedByIdx: (num: number) => `//div/div[${num}]/span`
 }
 export const ImportWalletPageTextInfo = {
-    getWorldByIdx: (num:number)=> `Word ${num}`,
-    password:"Password",
-    recovery:"Recovery"
+    getWorldByIdx: (num: number) => `Word ${num}`,
+    password: 'Password',
+    recovery: 'Recovery'
 }
 export const CreateANewWalletPageTextInfo = {
-    back:"Back",
-    next:"Next",
-    NewPassword:"New password(8 character min)",
-    ConfirmPassword:"Confirm password",
-    Create:"Create"
+    back: 'Back',
+    next: 'Next',
+    NewPassword: 'New password(8 character min)',
+    ConfirmPassword: 'Confirm password',
+    Create: 'Create'
 }
+
+export const WalletManagerPageTestIdInfo = {
+
+
+    /**
+     *  Page 1: New to nexus
+     *     test-id: createWallet on 'Create a wallet button'
+     *     test-id: importWallet on 'Import wallet button'
+     */
+    CreateWallet: 'createWallet',
+    ImportWallet: 'importWallet',
+
+
+    /**
+     *     Page 2: get started
+     *
+     *     test-id: getStarted, the 'get started' button
+     */
+
+    GetStarted: 'getStarted',
+
+    /**
+     *
+     *  Page 3 create account
+     *
+     *     test-id: userName, the 'user name' input
+     *     test-id: back，the 'back button'
+     *     test-id: next, the 'next' button
+     *
+     */
+
+    UserName: 'userName',
+    Back: 'back',
+    Next: 'next',
+
+
+    /**
+     * Page 4 create password
+     *
+     * test-id: password, the password input
+     * test-id: confirm-password, the confirm password input
+     * test-id: aggreeTermsOfUse the checkbox item
+     * test-id: back，the 'back button'
+     * test-id: next, the 'next' button
+     *
+     */
+
+    Password: 'password',
+    ConfirmPassword: 'confirm-password',
+    AggreeTermsOfUse: 'aggreeTermsOfUse',
+    // back
+    // next
+
+    /**
+     * Page 5, wallet generation seed
+     *  test-id: seed#n, the nth seed item
+     *  test-id: copyToClipboard, the copy to clipboard button
+     *  test-id: back，the 'back button'
+     *  test-id: next, the 'next' button
+     *
+     */
+
+    GetSeedByIdx: (num: number) => `seed#${num}`,
+    CopyToClipboard: 'copyToClipboard',
+    // back
+    // next
+
+    /**
+     * Page 6, confirm seed（TBD）
+     *
+     *     test-id: selectedSeed#n, when the user select a seed, the seed will appear in top of the page
+     *     test-id: seed#n, the seed waiting for select. a extra boolean attribute named seed-selected tells the seed selected status
+     *     test-id: back，the 'back button'
+     *     test-id: next, the 'next' button
+     */
+
+    getSelectedSeedByIdx: (num: number) => `selectedSeed#${num}`,
+    // seed#n
+    // back
+    // next
+
+    /**
+     * Page7, all down, conguratulations
+     *
+     *      test-id: done, the 'all done' button
+     */
+
+    Done: 'done',
+
+
+    /**
+     * Page 8, import wallet
+     *
+     *    test-id: seed#1, seed#2, ... seed#n the seed input for the recovery, n means the index
+     *    test-id: back，the 'back button'
+     *    test-id: next, the 'next' button
+     */
+
+}
+
+
